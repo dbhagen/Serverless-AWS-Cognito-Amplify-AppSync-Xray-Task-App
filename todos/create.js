ac691c3250c9a3abd46cebf11386bec00cc311a6
Daniel B Hagen <daniel.b.hagen@gmail.com>
2019-04-24 22:53:25 -0500

    Adding load testing scripts.

--- todos/create.js
+++ todos/create.js
@@ -1,21 +1,23 @@
-'use strict';
+/** @format */
 
-const uuid = require('uuid');
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+'use strict'
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid')
+const AWS = require('aws-sdk') // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
 module.exports.create = (event, context, callback) => {
-  const timestamp = new Date().getTime();
-  const data = JSON.parse(event.body);
+  const timestamp = new Date().getTime()
+  const data = JSON.parse(event.body)
   if (typeof data.text !== 'string') {
-    console.error('Validation Failed');
+    console.error('Validation Failed')
     callback(null, {
       statusCode: 400,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Couldn\'t create the todo item.',
-    });
-    return;
+      body: "Couldn't create the todo item."
+    })
+    return
   }
 
   const params = {
@@ -25,28 +27,28 @@ module.exports.create = (event, context, callback) => {
       text: data.text,
       checked: false,
       createdAt: timestamp,
-      updatedAt: timestamp,
-    },
-  };
+      updatedAt: timestamp
+    }
+  }
 
   // write the todo to the database
-  dynamoDb.put(params, (error) => {
+  dynamoDb.put(params, error => {
     // handle potential errors
     if (error) {
-      console.error(error);
+      console.error(error)
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the todo item.',
-      });
-      return;
+        body: "Couldn't create the todo item."
+      })
+      return
     }
 
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
-  });
-};
+      body: JSON.stringify(params.Item)
+    }
+    callback(null, response)
+  })
+}
